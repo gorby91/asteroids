b2c6127466218e347fcaf91a89cb15b04589377b
gorby91 <sam.voller@gmail.com>
2024-12-28 10:39:40 +1100

    refactor code after solution

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import pygame
 from constants import *
-pygame.init
 
 def main():
-	#pygame.init()
+	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print(type(screen))
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
